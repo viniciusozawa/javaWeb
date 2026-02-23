a9ffe982f930cb58855e73aada5941a46ab5ac78
VINICIUS YUJI OZAWA <02338079698@CAMPUS.MACHADO.MCH>
2026-02-23 14:59:53 -0300

    first commit

---
+++ src/main/java/br/edu/ifsuldeminas/viniciusozawainfoh/modelo/entity/Cidade.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifsuldeminas.viniciusozawainfoh.modelo.entity;
+
+/**
+ *
+ * @author 02338079698
+ */
+public class Cidade {
+    private Integer codCidade;
+    private String nomeCidade, ufCidade;
+
+    public Integer getCodCidade() {
+        return codCidade;
+    }
+
+    public void setCodCidade(Integer codCidade) {
+        this.codCidade = codCidade;
+    }
+
+    public String getNomeCidade() {
+        return nomeCidade;
+    }
+
+    public void setNomeCidade(String nomeCidade) {
+        this.nomeCidade = nomeCidade;
+    }
+
+    public String getUfCidade() {
+        return ufCidade;
+    }
+
+    public void setUfCidade(String ufCidade) {
+        this.ufCidade = ufCidade;
+    }
+    
+    
+}
---
+++ src/main/java/br/edu/ifsuldeminas/viniciusozawainfoh/servico/WebConstante.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifsuldeminas.viniciusozawainfoh.servico;
+
+/**
+ *
+ * @author 02338079698
+ */
+public class WebConstante {
+     public static final String BASE_PATH = "/br/edu/ifsuldeminas/viniciusozawainfoh/controller";
+}
