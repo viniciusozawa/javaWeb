5ed040e636cba2a300768687611414e548dbefec
VINICIUS YUJI OZAWA <02338079698@CAMPUS.MACHADO.MCH>
2026-03-02 14:55:50 -0300

    Aula 02/03/2026 3.0

--- src/main/java/br/edu/ifsuldeminas/viniciusozawainfoh/controller/CidadeControlador.java
+++ src/main/java/br/edu/ifsuldeminas/viniciusozawainfoh/controller/CidadeControlador.java
@@ -5,14 +5,35 @@
 package br.edu.ifsuldeminas.viniciusozawainfoh.controller;
 
 import br.edu.ifsuldeminas.viniciusozawainfoh.servico.WebConstante;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  *
  * @author 02338079698
  */
 
-@WebServlet(WebConstante.BASE_PATH+"/CidadeControlar")
-public class CidadeControlador {
+@WebServlet(WebConstante.BASE_PATH+"/CidadeControlador")
+//designar uma classe como um servelts, mapeando de URL no Servidor Web(GlassFish)
+public class CidadeControlador extends HttpServlet{
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        super.doGet(request, response); 
+        
+        try {
+            
+        } catch (NumberFormatException e) {
+            response.getWriter().println("Erro: um ou mais parametros não são número válido"+e.getMessage());
+        }catch (IllegalArgumentException ex){
+            response.getWriter().println("Erro: "+ex.getMessage());
+        }
+    }
+    
+    
     
 }
